5564d711a6dc45636b9aafa3bb2e6cc9f489d57e
Rafał Buchner <rafal.buchner@gmail.com>
2024-11-03 22:36:18 +0100

    add IntersectGlyphWithLine function to tools.py

---
+++ stemmath/tools.py
@@ -0,0 +1,6 @@
+def IntersectGlyphWithLine(
+    glyph,
+    guideline2: tuple[float | int],
+    canHaveComponent: bool = False,
+    addSideBearings: bool = False,
+) -> tuple[tuple[float | int]]: ...
